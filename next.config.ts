35bfefe95e9a8ffe196b040c799eab4ee8905cff
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-06-23 12:15:21 +0500

    Strapi

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  images: {
+    domains: ["fortunate-respect-d9ba68c326.strapiapp.comhttps", "fortunate-respect-d9ba68c326.media.strapiapp.com"]
+  }
 };
 
 export default nextConfig;
